2109eda6b377c8146c0ce58d668310edc9ed3b17
Mukhammad Khalimudin <khalimudin@gmail.com>
2018-08-10 06:21:24 +0700

    delete unused import

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This demo for spring boot with mybatis and h2 database embedded
+
+## Credits:
+
+VueJs: https://codepen.io/ethiel97/pen/mWLRaN
--- src/main/java/com/example/demomybatis/employee/EmployeeController.java
+++ src/main/java/com/example/demomybatis/employee/EmployeeController.java
@@ -3,10 +3,8 @@ package com.example.demomybatis.employee;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
